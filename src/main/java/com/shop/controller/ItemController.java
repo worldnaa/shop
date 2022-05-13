63c93977078fb637a500955acaa2821481962322
worldnaa <neuri93@naver.com>
2022-05-13 22:30:14 +0900

    -6장.상품등록/조회 완료

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -1,8 +1,13 @@
 package com.shop.controller;
 
 import com.shop.dto.ItemFormDto;
+import com.shop.dto.ItemSearchDto;
+import com.shop.entity.Item;
 import com.shop.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,6 +20,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -93,5 +99,29 @@ public class ItemController {
         }
 
         return "redirect:/";
+    }
+
+    //상품 관리 페이지 진입 (+조회한 상품 데이터를 화면에 전달)
+    @GetMapping(value = {"/admin/items", "/admin/items/{page}"})
+    public String itemManage(ItemSearchDto itemSearchDto, @PathVariable("page") Optional<Integer> page, Model model) {
+
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 3);
+        Page<Item> items = itemService.getAdminItemPage(itemSearchDto, pageable);
+
+        model.addAttribute("items", items);
+        model.addAttribute("itemSearchDto", itemSearchDto);
+        model.addAttribute("maxPage", 5);
+
+        return "item/itemMng";
+    }
+
+    //상품 상세 페이지 진입
+    @GetMapping(value = "/item/{itemId}")
+    public String itemDtl(Model model, @PathVariable("itemId") Long itemId) {
+
+        ItemFormDto itemFormDto = itemService.getItemDtl(itemId);
+        model.addAttribute("item", itemFormDto);
+
+        return "item/itemDtl";
     }
 }
---
+++ src/main/java/com/shop/repository/ItemRepositoryCustom.java
@@ -0,0 +1,15 @@
+package com.shop.repository;
+
+import com.shop.dto.ItemSearchDto;
+import com.shop.dto.MainItemDto;
+import com.shop.entity.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ItemRepositoryCustom {
+
+    Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+
+    //메인페이지에 보여줄 상품 리스트를 가져오는 메소드
+    Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+}
